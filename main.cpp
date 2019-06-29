cd18591fd0e5f7978bd42795267353e045fba0db
User <ivan09354@gmail.com>
2019-06-29 19:50:31 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "lib.h"
+
+#include <iostream>
+
+using namespace std;
+int main (int, char **)
+{
+    cout << "Hello, world!";
+    cout << "Version: " << version() << std::endl;
+
+    return 0;
+}
